e220d5b208e1ff425c8ae46ece778e9767f371b7
Catalin_Stoica <catalinstoica2105@yahoo.com>
2025-03-05 01:07:58 +0200

    synchro repo

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-  @Get()
-  getHello(): string {
-    return 'Serverul rulează cu succes! 🚀';
-  }
-}
--- src/survey/survey.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Survey } from './survey.entity';
-
-@EntityRepository(Survey)
-export class SurveyRepository extends Repository<Survey> {}
