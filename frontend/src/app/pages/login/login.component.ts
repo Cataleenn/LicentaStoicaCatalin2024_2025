7a2298ce742e29a8862d5274b2b7ac94695c28b9
Catalin_Stoica <catalinstoica2105@yahoo.com>
2025-03-20 00:11:00 +0200

    Add stylization to login page

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,37 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule], // ✅ Importă FormsModule și CommonModule
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
-onSubmit(event: Event) {
-  event.preventDefault();
-  this.authService.login(this.email, this.password).subscribe({
-    next: (response) => {
-      if (response?.access_token) {
-        this.authService.setToken(response.access_token);
-        this.router.navigate(['/admin-dashboard']); // ✅ Redirecționează corect
+  onSubmit(event: Event) {
+    event.preventDefault();
+    this.errorMessage = ''; // Resetare eroare
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        if (response?.access_token) {
+          this.authService.setToken(response.access_token);
+          this.router.navigate(['/admin-dashboard']);
+        }
+      },
+      error: (error) => {
+        console.error('❌ Eroare login:', error);
+        this.errorMessage = 'Email sau parolă incorectă!';
       }
-    },
-    error: (error) => {
-      console.error('❌ Eroare login:', error);
-    }
-  });
-}
-  
-  
-  
+    });
+  }
 }
